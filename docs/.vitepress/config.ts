86fe038bd1d40293be13b82a0b1795af9d40aada
MarkTang3611 <marktang3611@qq.com>
2025-11-01 14:43:57 +0800

    feat：完善大纲配置和个人简历文档。

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   description: '一个基于 VitePress 的个人博客',
   base: '/mark-tang-blog/',
   themeConfig: {
+    outline: {
+      level: [1, 2], // 默认值：显示 <h2> 和 <h3> 标题
+      label: '页面导航' // 标题名称，默认是 'On this page'
+    },
     nav: [
       { text: '首页', link: '/' },
       { text: '关于', link: '/about' },
@@ -13,12 +17,20 @@ export default defineConfig({
     ],
     sidebar: [
       {
+        text: 'Vue',
+        items: []
+      },
+      {
         text: 'electron',
         items: [
           { text: '使用electron+vue3+ts+vite搭建桌面端项目', link: '/posts/create-electron' },
           { text: '实现electron应用往windows注册表写注册项', link: '/posts/electron-registry' },
           { text: '基于electron应用实现截图', link: '/posts/print-screen' }
         ]
+      },
+      {
+        text: '其他',
+        items: []
       }
     ]
   }
